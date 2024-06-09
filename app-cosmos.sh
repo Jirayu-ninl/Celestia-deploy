0840b663c5aeaa96bd0c12ad843fc9f7aa224ced
TheIceJi <Jirayu.ninl@gmail.com>
2024-06-09 16:11:40 +0700

    [app:cosmos] update permissions

--- app-cosmos.sh
+++ app-cosmos.sh
@@ -2,14 +2,14 @@
 
 cd /home/celestia
 
-sudo rm -r apps/cosmos
-sudo rm -R tmp
+rm -r apps/cosmos
+rm -R tmp
 mkdir tmp
-sudo wget -i app-cosmos.txt -P tmp
+wget -i app-cosmos.txt -P tmp
 
 cd tmp
 cat celestia-cosmos.z01 celestia-cosmos.z02 celestia-cosmos.zip > celestia-cosmos-merge.zip
 
 cd ..
 unzip -o celestia-cosmos-merge.zip -d .
-sudo cp .env apps/cosmos/
+cp .env apps/cosmos/
